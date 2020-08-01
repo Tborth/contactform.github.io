c6f9fc670bc80997570154f34655ae6e7b9a3c70
Tborth <46168370+Tborth@users.noreply.github.com>
2020-08-01 17:38:29 +0530

    ddd

--- index.php
+++ index.php
@@ -1,64 +1,17 @@
 <?php
-    if(isset($_POST['submit']))
-    {
-        $name = $_POST['name']; // Get Name value from HTML Form
-        $email_id = $_POST['email']; // Get Email Value
-        $mobile_no = $_POST['mobile']; // Get Mobile No
-        $msg = $_POST['message']; // Get Message Value
-         
-        $to = "tarunchaudhary243@gmail.com"; // You can change here your Email
-        $subject = "'$name' has been sent a mail"; // This is your subject
-         
-        // HTML Message Starts here
-        $message ="
-        <html>
-            <body>
-                <table style='width:600px;'>
-                    <tbody>
-                        <tr>
-                            <td style='width:150px'><strong>Name: </strong></td>
-                            <td style='width:400px'>$name</td>
-                        </tr>
-                        <tr>
-                            <td style='width:150px'><strong>Email ID: </strong></td>
-                            <td style='width:400px'>$email_id</td>
-                        </tr>
-                        <tr>
-                            <td style='width:150px'><strong>Mobile No: </strong></td>
-                            <td style='width:400px'>$mobile_no</td>
-                        </tr>
-                        <tr>
-                            <td style='width:150px'><strong>Message: </strong></td>
-                            <td style='width:400px'>$msg</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </body>
-        </html>
-        ";
-        // HTML Message Ends here
-         
-        // Always set content-type when sending HTML email
-        $headers = "MIME-Version: 1.0" . "\r\n";
-        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
- 
-        // More headers
-        $headers .= 'From: Admin <admin@websapex.com>' . "\r\n"; // Give an email id on which you want get a reply. User will get a mail from this email id
-        $headers .= 'Cc: info@websapex.com' . "\r\n"; // If you want add cc
-        $headers .= 'Bcc: boss@websapex.com' . "\r\n"; // If you want add Bcc
-         
-        if(mail($to,$subject,$message,$headers)){
-            // Message if mail has been sent
-            echo "<script>
-                    alert('Mail has been sent Successfully.');
-                </script>";
-        }
- 
-        else{
-            // Message if mail has been not sent
-            echo "<script>
-                    alert('EMAIL FAILED');
-                </script>";
-        }
-    }
+if(!empty($_POST["send"])) {
+	$name = $_POST["userName"];
+	$email = $_POST["userEmail"];
+	$subject = $_POST["subject"];
+	$content = $_POST["content"];
+
+	$conn = mysqli_connect("localhost", "root", "test", "blog_samples") or die("Connection Error: " . mysqli_error($conn));
+	mysqli_query($conn, "INSERT INTO tblcontact (user_name, user_email,subject,content) VALUES ('" . $name. "', '" . $email. "','" . $subject. "','" . $content. "')");
+	$insert_id = mysqli_insert_id($conn);
+	//if(!empty($insert_id)) {
+	   $message = "Your contact information is saved successfully.";
+	   $type = "success";
+	//}
+}
+require_once "contact-view.php";
 ?>
